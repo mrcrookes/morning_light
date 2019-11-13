f5059262edc8567eac92b026b522d09a16df9fe0
mrcrookes <52488214+mrcrookes@users.noreply.github.com>
2019-11-13 18:44:26 +0000

    Create morning_light.py

---
+++ code/morning_light.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+from gpiozero import PWMLED
+from time import sleep
+from datetime import datetime
+from threading import Timer
+
+light = PWMLED(17)
+
+def wakeup():
+    for i in reversed(range(1, 5)):
+        
+        light.value = 1. / i*i       
+        sleep(300)      
+        print(i)
+
+def off():
+    
+    light.value = 0  
+    print("off")
+
+today = datetime.today()
+start = today.replace(day = today.day + 1, hour = 6, minute = 45, second = 0, microsecond = 0)
+stop = today.replace(day = today.day + 1, hour = 7, minute = 30, second = 0, microsecond = 0)
+
+start_diff = start - today
+stop_diff = stop - today
+
+on_secs = start_diff.seconds + 1
+off_secs = stop_diff.seconds + 1
+
+on = Timer(on_secs, wakeup)
+off = Timer(off_secs, off)
+
+on.start()
+off.start()
